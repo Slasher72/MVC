efd96747a39f82d142987e4af4f77da8d44eccf4
valentin.pean <valentin.pean@bts-malraux.net>
2014-11-07 11:15:19 +0000

    suite DAO

--- dao/DAO.java
+++ dao/DAO.java
@@ -1,5 +1,4 @@
 package dao;
-
 import java.sql.Connection;
 
 public abstract class DAO<T> {
@@ -7,4 +6,10 @@ public abstract class DAO<T> {
 	public static Connection connex ;
 
 	public abstract T create (T obj);
+
+	public abstract T update (T obj);
+
+	public abstract T read (int code);
+
+	public abstract void delete (T obj);
 }
